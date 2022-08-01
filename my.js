6ac9a1861e2e6589b2f9baf6fed8b3af6051dcb8
aondodawid <aondo1@gmail.com>
2022-08-01 22:18:28 +0200

    Repair of leyout and add the logic of app

--- my.js
+++ my.js
@@ -1,37 +1,93 @@
-function generatePassword() {
-  let firstInput = document.getElementById("firstPassword");
-  let secondInput = document.getElementById("secondPasword");
-  let thirdInput = document.getElementById("thirdPasword");
-  let fourthInput = document.getElementById("fourthPasword");
-  let messege = document.getElementById("message");
+const length = document.getElementById("passwordLength");
+const button = document.querySelector("button");
+const passwords = document.querySelectorAll(".input--password");
 
-  let length = document.getElementById("passworLength").value;
-  console.log(length);
+const specialchar = document.getElementById("special-char");
+const uppercase = document.getElementById("uperrcase");
 
-  // let password = "";
-  // let password = " ";
-  let password;
+const message = document.querySelector(".error-message");
 
-  let randomNumber;
-  let char;
+const randomItem = (arr) => arr[(Math.random() * arr.length) | 0];
 
-  function makePassword(length, word) {
-    if (length <= 0) {
-      return 1;
-    } else {
-      randomNumber = Math.floor(99 - Math.random() * 66);
-      char = String.fromCharCode(randomNumber);
-      word += char;
-      console.log(word);
-      return makePassword(length - 1);
-    }
+function randomChar() {
+  let randomNumber = Math.round(126 - Math.random() * 93);
+  let char = String.fromCharCode(randomNumber);
+  return char;
+}
+
+function makePassword(length, word) {
+  let arrayOfChar = generateString(+length);
+  const arraOfNum = [];
+
+  for (let i = 0; i < length; i++) {
+    arraOfNum.push(i);
   }
 
-  // console.log(length);
+  let Num = randomItem(arraOfNum);
 
-  // console.log(char);
-
-  firstInput.value = makePassword(length, password);
-  secondInput.value = makePassword(length);
-  console.log(password);
+  if (uppercase.value == 1 && specialchar.value == 1) {
+    for (let i = 0; i < length; i++) {
+      word += randomChar(length);
+    }
+  } else if (uppercase.value == 1 && specialchar.value == 0) {
+    let item = randomItem(arrayOfChar);
+    let item2 = randomItem(arrayOfChar);
+    for (let i = Num; i < length; i++) {
+      arrayOfChar.splice(arrayOfChar.indexOf(item), 1, item2.toUpperCase());
+    }
+    word += arrayOfChar.join("");
+  } else {
+    word += arrayOfChar.join("");
+  }
+  return word;
 }
+
+function generatePassword() {
+  let word = "";
+  message.classList.remove("visibility");
+  if (length.value > 0) {
+    passwords.forEach((password) => {
+      return (password.value = makePassword(length.value, word));
+    });
+  } else {
+    return displayErrorMessage();
+  }
+}
+
+function displayErrorMessage() {
+  message.classList.add("visibility");
+  message.classList.remove("message");
+  message.textContent = "Password must have at last one character";
+}
+
+function coppyMessage() {
+  message.classList.add("visibility");
+  message.classList.add("message");
+  message.textContent = "Password copied";
+}
+
+function copyToClipboard() {
+  /* Select the text field */
+
+  this.select();
+  this.setSelectionRange(0, 99999); /*For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(this.value);
+
+  /*display message*/
+  if (this.value != "") {
+    coppyMessage();
+  }
+}
+
+button.addEventListener("click", generatePassword);
+
+passwords.forEach((password) => {
+  password.addEventListener("click", copyToClipboard);
+});
+
+const generateString = (howLong) =>
+  Array(howLong)
+    .fill("")
+    .map((v) => Math.random().toString(36).charAt(2));
