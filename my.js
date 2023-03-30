e6b12d49474974f8505bbf0ca9c68bbac9e1ea56
aondodawid <aondo1@gmail.com>
2023-03-30 23:01:43 +0200

    Additional changes to generate password logic

--- my.js
+++ my.js
@@ -1,106 +1,95 @@
-function get(el) {
-  document.getElementById(el);
-}
+const get = (el) => document.getElementById(el);
 
-get("passwordLength");
+const specialChar = get('special-char');
+const upperCase = get('upperCase');
+const passwordLength = get('passwordLength');
+const button = document.querySelector('button');
+const errorMessage = document.querySelector('.error-message');
+const passwords = document.querySelectorAll('.input--password');
 
-var button = document.querySelector("button");
-const passwords = document.querySelectorAll(".input--password");
-const specialChar = document.getElementById("special-char");
-const upperCase = document.getElementById("upperCase");
+const randomItem = (arr) => arr[Math.floor(Math.random() * arr.length || 0)];
 
-const message = document.querySelector(".error-message");
-
-const randomItem = (arr) => arr[(Math.random() * arr.length) | 0];
+const generateString = (howLong) =>
+  Array(howLong)
+    .fill('')
+    .map(() => Math.random().toString(36).charAt(2));
 
 function randomChar() {
-  let randomNumber = Math.round(126 - Math.random() * 93);
-  let char = String.fromCharCode(randomNumber);
+  const randomNumber = Math.round(126 - Math.random() * 93);
+  const char = String.fromCharCode(randomNumber);
   return char;
 }
 
-function makePassword(length, word) {
-  let arrayOfChar = generateString(+length);
+function makePassword(passwordLength, word) {
+  const arrayOfChar = generateString(+passwordLength);
   const arrayOfNum = [];
+  const Num = Math.floor(Math.random() * arrayOfChar.length || 0);
+  let text = word;
 
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     arrayOfNum.push(i);
   }
 
-  let Num = randomItem(arrayOfNum);
-
-  if (upperCase.value == 1 && specialChar.value == 1) {
-    for (let i = 0; i < length; i++) {
-      word += randomChar(length);
+  if (upperCase.value === '1' && specialChar.value === '1') {
+    for (let i = 0; i < passwordLength; i++) {
+      text += randomChar(passwordLength);
     }
-  } else if (upperCase.value == 1 && specialChar.value == 0) {
-    let item = randomItem(arrayOfChar);
-    let item2 = randomItem(arrayOfChar);
-    for (let i = Num; i < length; i++) {
+  } else if (upperCase.value === '1' && specialChar.value === '0') {
+    const item = randomItem(arrayOfChar);
+    const item2 = randomItem(arrayOfChar);
+    for (let i = Num; i < passwordLength; i++) {
       arrayOfChar.splice(arrayOfChar.indexOf(item), 1, item2.toUpperCase());
     }
-    word += arrayOfChar.join("");
+    text += arrayOfChar.join('');
   } else {
-    word += arrayOfChar.join("");
+    text += arrayOfChar.join('');
   }
-  return word;
+  return text;
 }
 
 function generatePassword() {
-  let word = "";
-  message.classList.remove("visibility");
-  if (length.value <= 0) {
+  const word = '';
+  errorMessage.classList.remove('visibility');
+  if (passwordLength.value <= 0) {
     return displayErrorMessage();
-  } else if (length.value > 20) {
-    return displayErrorLengthMessage();
-  } else {
-    passwords.forEach((password) => {
-      return (password.value = makePassword(length.value, word));
-    });
   }
+  if (passwordLength.value > 20) {
+    return displayErrorLengthMessage();
+  }
+  passwords.forEach((password) => (password.value = makePassword(passwordLength.value, word)));
 }
 
 function displayErrorMessage() {
-  message.classList.add("visibility");
-  message.classList.remove("message");
-  message.textContent = "Password must have at last one character";
+  errorMessage.classList.add('visibility');
+  errorMessage.classList.remove('message');
+  errorMessage.textContent = 'Password must have at last one character';
 }
 
 function displayErrorLengthMessage() {
-  message.classList.add("visibility");
-  message.classList.remove("message");
-  message.textContent =
-    "Password will cause problems if more than 20 characters long";
+  errorMessage.classList.add('visibility');
+  errorMessage.classList.remove('message');
+  errorMessage.textContent = 'Password will cause problems if more than 20 characters long';
 }
 
 function copyMessage() {
-  message.classList.add("visibility");
-  message.classList.add("message");
-  message.textContent = "Password copied";
+  errorMessage.classList.add('visibility');
+  errorMessage.classList.add('message');
+  errorMessage.textContent = 'Password copied';
 }
 
 function copyToClipboard() {
-  /* Select the text field */
-
   this.select();
-  this.setSelectionRange(0, 99999); /*For mobile devices */
+  this.setSelectionRange(0, 99999);
 
-  /* Copy the text inside the text field */
   navigator.clipboard.writeText(this.value);
 
-  /*display message*/
-  if (this.value != "") {
+  if (this.value !== '') {
     copyMessage();
   }
 }
 
-button.addEventListener("click", generatePassword);
+button.addEventListener('click', generatePassword);
 
 passwords.forEach((password) => {
-  password.addEventListener("click", copyToClipboard);
+  password.addEventListener('click', copyToClipboard);
 });
-
-const generateString = (howLong) =>
-  Array(howLong)
-    .fill("")
-    .map((v) => Math.random().toString(36).charAt(2));
